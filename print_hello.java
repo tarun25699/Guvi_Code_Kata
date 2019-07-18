31a86656bf33eb86d0ebf172384b45b05c4913e8
Tarun Bapanapalli <tarunbapanapalli1999@gmail.com>
2019-07-18 21:59:42 +0530

    Create print_hello.java

---
+++ print_hello.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Main
+{
+public static void main(String args[]){
+Scanner inp=new Scanner(System.in);
+int n=inp.nextInt();
+for(int i=1;i<=n;i++)
+System.out.println("Hello");
+}
+}
